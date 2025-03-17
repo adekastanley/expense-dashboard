0710c80c723410503e3fd02baf61d7b9a8390ed6
adeka stanley <adekastanley2@gmail.com>
2025-03-17 14:34:24 +0100

    homepage update

--- src/components/pages/homepage/hero.tsx
+++ src/components/pages/homepage/hero.tsx
@@ -2,14 +2,21 @@ import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
 	return (
+		// <section className="h-screen w-full  bg-no-repeat bg-cover bg-center ">
 		<section className="h-screen w-full bg-red bg-[url(/assets/backgrounds/bg1.svg)] bg-no-repeat bg-cover bg-center ">
 			<div className="w-full h-full flex flex-col gap-5 justify-center relative items-center ">
-				<h1 className="text-3xl lg:text-7xl text-white font-bold  text-center">
+				<h1 className="text-3xl lg:text-7xl text-black font-bold  text-center">
 					Track what matters <br />
-					in less time
+					<span className="text-primary"> in less time</span>
 				</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda,
+					quia.
+				</p>
 				<div className="gap-5 flex ">
-					<Button size={"lg"}>Buy now</Button>
+					<Button className="" size={"lg"}>
+						Buy now
+					</Button>
 					<Button size={"lg"}>Try it out</Button>
 				</div>
 			</div>
