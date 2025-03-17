7a6b2dead17353607db0acb8fd3ff75e6f56342a
adeka stanley <adekastanley2@gmail.com>
2025-03-17 16:36:02 +0100

    update

--- src/components/global/navigation/MobileNav.tsx
+++ src/components/global/navigation/MobileNav.tsx
@@ -3,7 +3,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
 	Sheet,
-	SheetClose,
+	// SheetClose,
 	SheetContent,
 	SheetDescription,
 	SheetFooter,
--- src/components/global/navigation/navbar.tsx
+++ src/components/global/navigation/navbar.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import MobileNav from "./MobileNav";
 import PcNav from "./PcNav";
 
